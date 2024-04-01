9887ed2496fa80ab74ecc22a16b4f3ab73317a51
everyofflineuser [WSL] <nealeks18@mail.ru>
2024-04-01 10:22:23 +0900

    Fix

--- kernel.asm
+++ kernel.asm
@@ -3,8 +3,8 @@ section .text
         ;multiboot spec
         align 4
         dd 0x1BADB002              ;magic
-        dd 0x12                    ;flags
-        dd - (0x1BADB002 + 0x12)   ;checksum. m+f+c should be zero
+        dd 0x00                    ;flags
+        dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
 
 global start
 global keyboard_handler
